467a21bd3a273655b4ff6a8d9ca62bb977dbdf71
swift <swift@e2r10p14.42.fr>
2014-12-20 18:22:52 +0100

    added map view exercise 04

--- Rush42/Rush42/SecondViewController.swift
+++ Rush42/Rush42/SecondViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, MKMapViewDelegate {
 
+    /*
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 1
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        //mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        //mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
