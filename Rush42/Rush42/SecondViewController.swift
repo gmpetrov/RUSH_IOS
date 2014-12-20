2413f7c7fea49fc855dcd8e463611386ac9dcf3d
swift <swift@e2r10p12.42.fr>
2014-12-20 19:53:24 +0100

    ex06 Finished

--- Rush42/Rush42/SecondViewController.swift
+++ Rush42/Rush42/SecondViewController.swift
@@ -17,24 +17,31 @@ class SecondViewController: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }*/
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 1
         let location = CLLocationCoordinate2D(
-            latitude: 51.50007773,
-            longitude: -0.1246402
+            latitude: 48.896696,
+            longitude: 2.318451
+
         )
         // 2
-        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let span = MKCoordinateSpanMake(0.01, 0.01)
         let region = MKCoordinateRegion(center: location, span: span)
-        //mapView.setRegion(region, animated: true)
+        mapView.setRegion(region, animated: true)
         
         //3
         let annotation = MKPointAnnotation()
         annotation.setCoordinate(location)
-        annotation.title = "Big Ben"
-        annotation.subtitle = "London"
-        //mapView.addAnnotation(annotation)
+        annotation.title = "Ecole 42"
+        annotation.subtitle = "Pirate School"
+        mapView.addAnnotation(annotation)
+        
+        mapView.delegate = self
+        mapView.mapType = MKMapType.Satellite
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
